486233e0a684ec941d0083a72d58079fd0b3739a
Sunitha Muthukrishna <3684166+mksuni@users.noreply.github.com>
2024-11-10 19:28:26 -0800

    changes

---
+++ Notebook1.Notebook/notebook-content.py
@@ -0,0 +1,23 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "be7c0c25-b982-4cae-8e53-f5474f4d9793",
+# META       "default_lakehouse_name": "lakehousedemo1",
+# META       "default_lakehouse_workspace_id": "2dd73683-9728-46e9-9f74-6bf72fd01d8c"
+# META     }
+# META   }
+# META }
+
+
+# CELL ********************
+numbers = [1, 2, 3, 4, 5]  # Replace this with your array of numbers
+
+# Calculate the sum of the numbers
+sum_of_numbers = sum(numbers)
+
+# Print the sum
+print("The sum of the numbers is:", sum_of_numbers)
